98dd27421bdd1c152bea553ca6e785b23a0de9db
kimheonseung <khs920210@gmail.com>
2022-03-28 17:35:26 +0900

    server-depth-need_map

--- client/src/department/map/DepartmentMapHelper.js
+++ client/src/department/map/DepartmentMapHelper.js
@@ -3,10 +3,79 @@ import axios from "axios";
 
 const departmentTreeApiUrl = process.env.REACT_APP_API_URL_PREFIX+"department/tree";
 
+const style = [
+    {
+        selector: 'node',
+        style: {
+            // shape: 'hexagon',
+            'background-color': 'lightgreen',
+            'width': '25px',
+            'height': '25px',
+            // 'background-image': 'url(/icon/manager.gif)',
+            'label': 'data(label)'
+
+        }
+    },
+    {
+        selector: 'edge',
+        style: {
+            label: 'data(label)'
+        }
+    }
+];
+
+const layout = {
+    name: 'grid',
+};
+
+let nodesAndEdges = [];
+
+let x = 100;
+let y = 100;
+let depth = 0;
+
+const addDepartmentNodeRecursive = (arr, node) => {
+    const hasParent = node.department;
+    if(hasParent) {
+        const parentNode = node.department;
+        nodesAndEdges.push({
+            data: {
+                id: 'e-'+node.dataId,
+                label: parentNode.name,
+                source: 'n-'+parentNode.id,
+                target: 'n-'+node.dataId,
+            },
+            group: 'edges'
+        });
+        depth += 1;
+    }
+    arr.push({
+        data: {
+            id: 'n-'+node.dataId,
+            label: node.name,
+        },
+        position: {
+            x: hasParent ? x+50*(depth-1) : x+50*depth,
+            y: hasParent ? y+50*depth : y+50*(depth-1),
+        },
+        group: 'nodes'
+    });
+    if(node.children.length > 0) {
+        node.children.forEach(childNode => {
+            addDepartmentNodeRecursive(arr, childNode);
+        })
+    }
+}
+
 export const createDepartmentCytoscape = (el) => {
-    return new cytoscape({
-        container: el
+    let cy = new cytoscape({
+        container: el,
+        style: style,
+        layout:layout
     })
+    cy.layout({ name: 'preset' }).run();
+
+    return cy;
 }
 
 export const initDepartmentMap = (cy, treeData) => {
@@ -15,8 +84,13 @@ export const initDepartmentMap = (cy, treeData) => {
         .then((rs) => {
             const result = rs.data.status;
             if(result == 200) {
-                const treeArray = rs.data;
-                
+                nodesAndEdges = [];
+                const nodeList = rs.data.data;
+                nodeList.forEach(node => {
+                    addDepartmentNodeRecursive(nodesAndEdges, node);
+                });
+                cy.add(nodesAndEdges);
+
             }
         })
 }
